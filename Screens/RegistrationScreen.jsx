0700222251c640f786b23a06710707180f496f3f
VickyBondarenko <quikyv@gmail.com>
2023-06-20 13:47:23 +0200

    change style

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,29 +1,10 @@
 import React, { useState } from "react";
-import { Text, TextInput, View, Button } from "react-native";
+import { Text, TextInput, View, Button, ImageBackground } from "react-native";
 import { Formik } from "formik";
 import * as yup from "yup";
 import styled from "styled-components/native";
 
-const StyledButton = styled.Button`
-  background-color: orange;
-  color: #fff;
-  padding: 10px 20px;
-  border-radius: 5px;
-`;
-
 const FormWrapper = styled.View``;
-
-const Input = styled.TextInput`
-  height: 50px;
-  width: 343px;
-  padding: 16px;
-  border-radius: 8px;
-  border-width: 1px;
-  border-style: solid;
-  border-color: #e8e8e8;
-  background: #f6f6f6;
-  margin-bottom: 16px;
-`;
 
 const RegistrationScreen = () => {
   const validationSchema = yup.object().shape({
@@ -39,21 +20,11 @@ const RegistrationScreen = () => {
       .required("Please enter your password"),
   });
 
-  const handleSubmit = (values) => {
-    console.log({
-      avatar: values.avatar,
-      login: values.login,
-      email: values.email,
-      password: values.password,
-    });
-  };
+  const handleSubmit = (values) => console.log(values);
 
   return (
-    <RegistarationWrapper>
-      <Image
-        source={require("../assets/images/photoBG.png")}
-        // resizeMode="cover"
-      >
+    <RegistrationWrapper>
+      <ImageBG source={require("../assets/images/photoBG.png")}>
         <View></View>
         <ContentWrapper>
           <PageTitle>Реєстрація</PageTitle>
@@ -95,34 +66,34 @@ const RegistrationScreen = () => {
           </Formik>
           <LoginLink>Вже є акаунт? Увійти</LoginLink>
         </ContentWrapper>
-      </Image>
-    </RegistarationWrapper>
+      </ImageBG>
+    </RegistrationWrapper>
   );
 };
 export default RegistrationScreen;
 
-const RegistarationWrapper = styled.View`
+const RegistrationWrapper = styled.View`
   flex: 1;
+  width: 100%;
   justify-content: center;
   align-items: center;
 `;
 
-const Image = styled.ImageBackground`
-  flex: 1;
+const ImageBG = styled.ImageBackground`
+  width: 100%;
   justify-content: center;
   resize: cover;
 `;
 
 const ContentWrapper = styled.View`
   margin-top: 235px;
-  /* padding: 16px; */
-  padding: 92px 0 66px;
+  padding: 92px 16px 66px;
   background-color: #ffffff;
   border-radius: 25px 25px 0px 0px;
 `;
 
 const PageTitle = styled.Text`
-  padding: 32px 0;
+  padding-bottom: 32px;
   font-family: "Roboto";
   font-style: normal;
   font-weight: 500;
@@ -132,6 +103,17 @@ const PageTitle = styled.Text`
   /* letter-spacing: 0.01; */
 
   color: #212121;
+`;
+const Input = styled.TextInput`
+  height: 50px;
+  width: 100%;
+  padding: 16px;
+  border-radius: 8px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #e8e8e8;
+  background: #f6f6f6;
+  margin-bottom: 16px;
 `;
 
 const SubmittButton = styled.TouchableOpacity`
@@ -157,8 +139,6 @@ const LoginLink = styled.Text`
   font-weight: 400;
   font-size: 16px;
   line-height: 19px;
-
   text-align: center;
-
   color: #1b4371;
 `;
