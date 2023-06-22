59339a495dba445cda2d89562e57b841dab039f0
VickyBondarenko <quikyv@gmail.com>
2023-06-22 16:21:12 +0200

    Add userInfo component

--- Screens/PostScreen/PostsScreen.jsx
+++ Screens/PostScreen/PostsScreen.jsx
@@ -10,70 +10,31 @@ import {
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
+import UserInfo from "./UserInfo";
+import UserInfoImage from "../../assets/images/userFoto.png";
 
 const PostsScreen = () => {
-  const navigation = useNavigation();
+  console.log("UserInfo", UserInfo);
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <LogInWrapper>
-        <ImageBG source={require("../../assets/images/photoBG.png")}>
-          <ContentWrapper>
-            <PageTitle>Увійти</PageTitle>
-            <TextInput />
-            <LoginLink onPress={() => navigation.navigate("CommentsScreen")}>
-              <Text>Comments</Text>
-            </LoginLink>
-          </ContentWrapper>
-        </ImageBG>
-      </LogInWrapper>
-    </TouchableWithoutFeedback>
+    <>
+      <ScreenWrapper>
+        <UserInfo
+          fotoURL={{ uri: { UserInfoImage } }}
+          name="Natali Romanova"
+          email="email@example.com"
+        />
+      </ScreenWrapper>
+    </>
   );
 };
 export default PostsScreen;
 
-const LogInWrapper = styled.View`
+const ScreenWrapper = styled.View`
   flex: 1;
   width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ImageBG = styled.ImageBackground`
-  flex: 1;
-  width: 100%;
-  /* height: 100%; */
-  justify-content: center;
-  resize: cover;
-`;
-
-const ContentWrapper = styled.View`
-  margin-top: 295px;
-  padding: 32px 16px 144px;
+  /* justify-content: center;
+  align-items: center; */
   background-color: #ffffff;
-  border-radius: 25px 25px 0px 0px;
-`;
-
-const PageTitle = styled.Text`
-  /* margin-top: 92px; */
-  padding-bottom: 32px;
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 30px;
-  line-height: 35px;
-  text-align: center;
-  /* letter-spacing: 0.01; */
-
-  color: #212121;
-`;
-
-const LoginLink = styled.TouchableOpacity`
-  margin-top: 16px;
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 19px;
-  text-align: center;
-  color: #1b4371;
+  padding-left: 16px;
+  padding-right: 16px;
 `;
---
+++ Screens/PostScreen/UserInfo.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+import styled from "styled-components/native";
+
+const UserInfo = ({ fotoURL, name, email }) => {
+  return (
+    <UserInfoWrapper>
+      <Image
+        source={{ fotoURL }}
+        style={{
+          width: 60,
+          height: 60,
+          borderRadius: 16,
+          backgroundColor: "orange",
+        }}
+      />
+      <UserInfoTextWrapper>
+        <UserName>{name}</UserName>
+        <UserEmail>{email}</UserEmail>
+      </UserInfoTextWrapper>
+    </UserInfoWrapper>
+  );
+};
+
+export default UserInfo;
+
+const UserInfoWrapper = styled.View`
+  flex: 1;
+  flex-direction: row;
+  height: 60px;
+  padding-top: 32px;
+  padding-bottom: 32px;
+`;
+
+const UserInfoTextWrapper = styled.View`
+  flex: 1;
+  padding-left: 8px;
+  padding-top: 12px;
+`;
+
+const UserName = styled.Text`
+  color: #212121;
+  font-size: 13px;
+  font-family: Roboto;
+  font-weight: 700;
+`;
+const UserEmail = styled.Text`
+  color: rgba(33, 33, 33, 0.8);
+  font-size: 11px;
+  font-family: Roboto;
+`;
