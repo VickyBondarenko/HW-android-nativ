bb0bb8a5f9959a2b93d707a8928c9d72912253ad
VickyBondarenko <quikyv@gmail.com>
2023-06-26 11:29:06 +0200

    Add location screen with current locatiion

--- Screens/MapScreen/MapScreen.jsx
+++ Screens/MapScreen/MapScreen.jsx
@@ -1,22 +1,98 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 import styled from "styled-components/native";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 
-function MapScreen() {
-  const navigation = useNavigation();
+// const { GOOGLE_MAPS_API_KEY } = process.env;
+provider = PROVIDER_GOOGLE;
+
+const MapScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [placeName, setPlaceName] = useState("");
+
+  useEffect(() => {
+    let locationSubscription;
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+      }
+
+      locationSubscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          distanceInterval: 10,
+        },
+        async (newLocation) => {
+          const coords = {
+            latitude: newLocation.coords.latitude,
+            longitude: newLocation.coords.longitude,
+          };
+          setLocation(coords);
+
+          const geocode = await Location.reverseGeocodeAsync(coords);
+          if (geocode.length > 0) {
+            const { city } = geocode[0];
+            setPlaceName(city);
+          }
+        }
+      );
+    })();
+
+    return () => {
+      if (locationSubscription) {
+        locationSubscription.remove();
+      }
+    };
+  }, []);
+
   return (
-    <MapScreenWrapper>
-      <Text>MapScreen</Text>
-    </MapScreenWrapper>
+    <MapWrapper>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          ...location,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+        mapType="standard"
+        minZoomLevel={15}
+        onMapReady={() => console.log({ location })}
+        // onRegionChange={() => console.log("Region change")}
+      >
+        {location && (
+          <Marker
+            title={placeName !== "" ? placeName : "I am here"}
+            coordinate={location}
+            description="Photo location"
+          />
+        )}
+      </MapView>
+    </MapWrapper>
   );
-}
+};
+
+const MapWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+`;
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
 
 export default MapScreen;
-
-const MapScreenWrapper = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  /* height: 100%; */
-`;
--- test.jsx
+++ test.jsx
@@ -1,110 +1,89 @@
-<Tab.Navigator
-  screenOptions={({ route, navigation }) => ({
-    title: "Публікації",
-    headerStyle: {
-      borderBottomColor: "#E8E8E8",
-      borderBottomWidth: 1,
-      height: 60,
-      backgroundColor: "#FFFFFF",
-    },
-    headerTintColor: "#212121",
-    headerTitleAlign: "center",
-    headerTitleStyle: {
-      fontWeight: "bold",
-      fontSize: 17,
-      fontWeight: 500,
-      letterSpacing: -0.408,
-    },
-    headerRight: () => {
-      if (route.name === "Posts") {
-        return (
-          <LogoutButton onPress={() => alert("This is a button!")}>
-            <LogoutSVG />
-          </LogoutButton>
-        );
-      } else {
-        return null;
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import styled from "styled-components/native";
+import { PROVIDER_GOOGLE } from "react-native-maps";
+
+const { GOOGLE_MAPS_API_KEY } = process.env;
+provider = PROVIDER_GOOGLE;
+
+const MapScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [placeName, setPlaceName] = useState("");
+
+  useEffect(() => {
+    let locationSubscription;
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
       }
-    },
-    headerLeft: () => {
-      if (route.name === "CreatePost" || route.name === "Comments") {
-        return (
-          <TouchableOpacity
-            style={{ marginLeft: 16 }}
-            onPress={() => navigation.goBack()}
-          >
-            <Ionicons name="ios-arrow-back" size={24} color="#212121" />
-          </TouchableOpacity>
-        );
-      } else {
-        return null;
+
+      locationSubscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          distanceInterval: 10,
+        },
+        async (newLocation) => {
+          const coords = {
+            latitude: newLocation.coords.latitude,
+            longitude: newLocation.coords.longitude,
+          };
+          setLocation(coords);
+
+          // Получение информации о месте на основе координат
+          const geocode = await Location.reverseGeocodeAsync(coords);
+          if (geocode.length > 0) {
+            const { city } = geocode[0];
+            setPlaceName(city);
+          }
+        }
+      );
+    })();
+
+    return () => {
+      if (locationSubscription) {
+        locationSubscription.remove();
       }
-    },
-    tabBarStyle: {
-      height: 60,
-      paddingRight: 90,
-      paddingLeft: 90,
-      paddingTop: 9,
-      paddingBottom: 0,
-    },
-    tabBarIcon: ({ focused, color, size }) => {
-      let iconName;
-      if (route.name === "Posts") {
-        iconName = "grid-outline";
-      } else if (route.name === "CreatePost") {
-        iconName = "ios-add-sharp";
-      } else if (route.name === "UserProfile") {
-        iconName = "person-outline";
-      }
-      return <Ionicons name={iconName} size={size} color={color} />;
-    },
-    tabBarActiveBackgroundColor: "#FF6C00",
-    tabBarItemStyle: {
-      height: 40,
-      borderRadius: 20,
-    },
-    tabBarContentContainerStyle: {
-      flexDirection: "row",
-      justifyContent: "space-around",
-      alignItems: "center",
-      width: 70,
-    },
-    tabBarShowLabel: false,
-    tabBarActiveTintColor: "#FFFFFF",
-    tabBarInactiveTintColor: "gray",
-  })}
->
-  <Tab.Screen
-    name="Posts"
-    component={Posts}
-    options={{
-      headerRight: () => (
-        <LogoutButton onPress={() => alert("Sure?")}>
-          <LogoutSVG />
-        </LogoutButton>
-      ),
-    }}
-  />
-  <Tab.Screen
-    name="CreatePost"
-    component={CreatePost}
-    options={{
-      title: "Створити публікацію",
-      tabBarStyle: { display: "none" },
-    }}
-  />
-  <Tab.Screen
-    name="UserProfile"
-    component={UserProfileScreen}
-    options={{ headerShown: false, tabBarVisible: false }}
-  />
-  <Tab.Screen
-    name="Comments"
-    component={CommentsScreen}
-    options={{
-      tabBarVisible: false,
-      title: "Коментарі",
-      tabBarStyle: { display: "none" },
-    }}
-  />
-</Tab.Navigator>;
+    };
+  }, []);
+
+  return (
+    <MapWrapper>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          ...location,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+      >
+        {location && (
+          <Marker
+            title={placeName !== "" ? placeName : "I am here"}
+            coordinate={location}
+            description="Hello"
+          />
+        )}
+      </MapView>
+    </MapWrapper>
+  );
+};
+
+const MapWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+`;
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
+
+export default MapScreen;
